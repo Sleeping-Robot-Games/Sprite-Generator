2d7c42cebf36933ddf818d9b4066dfda0690b678
Bryan Reed <bryan87reed@gmail.com>
2022-01-19 08:40:30 -0700

    Sprite list working and spaced

--- Scripts/SpriteListItem.gd
+++ Scripts/SpriteListItem.gd
@@ -1,16 +1,18 @@
 extends HBoxContainer
 
 var sprite_data
+var current_animation = 0
+var animations = ['idle_front', 'idle_right', 'idle_back', 'idle_left']
 
 
 func init(_sprite_data):
 	sprite_data = _sprite_data
 	$SpriteName.text = sprite_data.name
 	g.load_sprite($SpriteContainer/PlayerSprites/SpriteHolder, sprite_data)
-
+	
 
 func _ready():
-	pass # Replace with function body.
+	$SpriteContainer/PlayerSprites/AnimationPlayer.play(animations[current_animation])
 
 
 func _on_Edit_button_up():
@@ -18,3 +20,9 @@ func _on_Edit_button_up():
 
 func _on_Delete_button_up():
 	g.remove_sprite_by_name(sprite_data.name)
+
+func _on_Turn_button_up(direction):
+	current_animation += direction
+	if current_animation == 4 or current_animation == -4:
+		current_animation = 0
+	$SpriteContainer/PlayerSprites/AnimationPlayer.play(animations[current_animation])
